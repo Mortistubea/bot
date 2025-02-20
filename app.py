7095cbd5b1adf6cc007de4dacb75fb97496faee2
Mortistubea <155967262+Mortistubea@users.noreply.github.com>
2025-02-20 19:11:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import asyncio
 from aiogram import executor
+
 from loader import dp
 import middlewares, filters, handlers
+
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
-from handlers.users.start import send_prayer_times  # Namoz vaqtlarini yuborish funksiyasini import qilish
 
 
 async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
+
     await on_startup_notify(dispatcher)
-    asyncio.create_task(send_prayer_times())  # Har kuni 20:00 da xabar yuborish
 
 
 if __name__ == '__main__':
